c69cb10b69958d8ceb8e3206c8d1163e3679d200
bengris32 <bengris32@protonmail.ch>
2022-09-14 18:30:44 +0100

    camellia: Build default Thermal HIDL
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: If56c0374ca1040be7c0724e4ed4987544a258223
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>
    Signed-off-by: fjrbutuhthr gass <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -404,8 +404,9 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@2.0.vendor
+    android.hardware.thermal@1.0.vendor \
+    android.hardware.thermal@2.0.vendor \
+    android.hardware.thermal@1.0-impl
 
 # USB
 PRODUCT_PACKAGES += \
